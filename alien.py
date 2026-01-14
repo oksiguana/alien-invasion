600272c4309672ee7fef2df1d2961f4928eff837
iguana <fedorchenkooksana10@gmail.com>
2026-01-14 12:09:37 +0100

    Creating an Instance of the Alien

--- alien.py
+++ alien.py
@@ -6,7 +6,7 @@ class Alien(Sprite):
 
     def __init__(self, ai_game):
         """Initialize the alien and set its starting position."""
-        super().__init__
+        super().__init__()
         self.screen = ai_game.screen
 
         # Load the alien image and set its rect attribute.
