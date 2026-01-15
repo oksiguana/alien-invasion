e1b4af37d20afff584a904ba15d0082f132a2310
iguana <fedorchenkooksana10@gmail.com>
2026-01-15 12:38:51 +0100

    making the fleet move to the right

--- settings.py
+++ settings.py
@@ -11,6 +11,9 @@ class Settings:
         # Ship settings
         self.ship_speed = 1.5
 
+        # Alien settings
+        self.alien_speed = 1.0
+
         # Bullet settings
         self.bullet_speed = 2.0
         self.bullet_width = 3
